bd19d6988634392e4b24a4e2e0225ee8e6861509
Joel Hill <joelhill@pop-os.localdomain>
2020-07-27 19:37:44 -0600

    Initialization of the console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace July_2020_dotnet_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
